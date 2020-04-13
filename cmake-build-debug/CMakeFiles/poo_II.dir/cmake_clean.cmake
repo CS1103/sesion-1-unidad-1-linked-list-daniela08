e8a142248e8a3178e09fb42aa835d987fc58995e
Daniela <52041953+daniela08@users.noreply.github.com>
2020-04-12 23:56:51 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/poo_II.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/poo_II.dir/linked_list.cpp.o"
+  "CMakeFiles/poo_II.dir/main.cpp.o"
+  "poo_II"
+  "poo_II.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/poo_II.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
